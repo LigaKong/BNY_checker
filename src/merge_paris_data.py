4a9b87d8411d57de6c2558b25acc1e73eb79779f
Liga Kong <ligakong@gmail.com>
2025-02-27 15:20:03 +0800

    update

--- src/merge_paris_data.py
+++ src/merge_paris_data.py
@@ -25,43 +25,36 @@ class MergeParisData:
             if not os.path.exists(self.returnsaudit_filepath):
                   raise FileNotFoundError(f"File {self.returnsaudit_filepath} not found.")
             
-      # Define a function to extract the number inside the last parentheses and convert it to an integer
-      def extract_parisid(x):
-            matches = re.findall(r"\((\d+)\)$", x)  # Match the number inside the last parentheses
-            if len(matches) > 0:
-                  return int(matches[-1])  # Get the last matched number
-            else:
-                  return None
-
-      def merge_paris_data(self):
-
-            df = pd.read_excel(self.returnsaudit_filepath, skiprows=4, header=0)
-
-            # Organize Returns Audit data
-            df = df.fillna(method='backfill',axis=1)
-            df['ParisID'] = df['Plan'].apply(MergeParisData.extract_parisid)
+      def extract_parisid(self, x):
+            matches = re.findall(r"\((\d+)\)$", str(x))
+            return int(matches[-1]) if matches else None
+      
+      def process_returns_audit(self, df):
+            df['ParisID'] = df['Plan'].apply(self.extract_parisid)
             df.rename(columns={'Prior\nMarket Value': 'Prior Market Value'}, inplace=True)
             df = df[self.returnsaudit_useful_columns].dropna(axis=0, how='all') # Keep useful columns and delete rows where all values are na
-                  
-            df_info = pd.read_excel(self.accountsetupaudit_filepath, skiprows=1, header=0)
-            
-            # Organize Account Setup Audit data.
-            df_info = df_info[self.accountsetupaudit_useful_columns]
-            df_info.rename(columns={"AccountId": 'ParisID'}, inplace=True)
-            df_info['ParisID'] = df_info['ParisID'].apply(lambda x: int(x))
-            
-            # Merge two dataframes.
-            df = pd.merge(df, df_info, on='ParisID', how='left')
-            
-            # Organize merge data.
-            df.drop_duplicates(subset=['ParisID'], keep='first', inplace=True)
-            df = df[(df['AccountType'] == 'Atomic') | (df['AccountType'].isnull())]
+            return df
 
+      def process_account_setup_audit(self, df_info):
+            df_info = df_info[self.accountsetupaudit_useful_columns].copy()
+            df_info.rename(columns={"AccountId": 'ParisID'}, inplace=True)
+            df_info['ParisID'] = df_info['ParisID'].astype(int)
+            return df_info
+      
+      def merge_paris_data(self):
+
+            df = pd.read_excel(self.returnsaudit_filepath, skiprows=4, header=0).fillna(method='backfill', axis=1)
+            df = self.process_returns_audit(df)
+            
+            df_info = pd.read_excel(self.accountsetupaudit_filepath, skiprows=1, header=0)
+            df_info = self.process_account_setup_audit(df_info)
+            
+            df = pd.merge(df, df_info, on='ParisID', how='left').drop_duplicates(subset=['ParisID'], keep='first')
+            df = df[(df['AccountType'] == 'Atomic') | (df['AccountType'].isnull())]
       
             # Remove commas from floating columns
             for col in self.float_columns:
-                  df[col] = df[col].astype(str)
-                  df[col] = df[col].apply(lambda x: float(x.replace(',', '')))
+                  df[col] = df[col].astype(str).apply(lambda x: float(x.replace(',', '')))
             
             # Strip leading and trailing whitespaces from string columns
             for col in self.str_columns:
@@ -71,19 +64,16 @@ class MergeParisData:
             df['CustodianSecurityID'] = df['CustodianSecurityID'].apply(lambda x: x if x == 'nan' else x.zfill(9)) 
 
             # Judge the Commingle Fund by CustodianAcct 
-            for num in df.index:
-                  if df.loc[num,'CustodianAcct'] == 'nan':
-                        pass
-                  elif list(df['CustodianAcct']).count(df.loc[num,'CustodianAcct']) > 1:
-                        df.loc[num,'Commingle Fund'] = 'Yes'
-                  else:
-                        df.loc[num,'Commingle Fund'] = 'No'    
+            df['Commingle Fund'] = df['CustodianAcct'].apply(lambda x: 'Yes' if df['CustodianAcct'].eq(x).sum() > 1 else 'No' if x != 'nan' else '')  
             
             return df
 
 if __name__ == '__main__':
 
-      client_name = "NEXCOM01"
-      df = MergeParisData(client_name).merge_paris_data()
-      print(df)
-      # df.set_index('ParisID').to_csv(f'./output/{client_name}.csv')
+      client_name = "FSM01"
+      try:
+            df = MergeParisData(client_name).merge_paris_data()
+            print(df)
+            # df.set_index('ParisID').to_csv(f'./output/{client_name}.csv')
+      except FileNotFoundError as e:
+            print(e)
